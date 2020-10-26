5f95859c0341b5330d7a6fe78e991a1cc1c836f2
Timothy Gramnaes <timothy.gramnaes@medieinstitutet.se>
2020-10-26 15:38:32 +0100

    First version of promptgame

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function startGame() {
+
+var name = prompt("And who the hell might you be?");
+
+var choice1 = prompt("Well then "+name+" would you like a beer or an insult?");
+
+alert("you chose "+choice1);
+
+}
